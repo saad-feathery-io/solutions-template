40f194b4b5c55f5d7aeacfbf41cbc115d12f3346
Saad <saad@feathery.io>
2025-03-18 13:37:16 -0400

    add examples

--- build/plugin.AddSrcComment.js
+++ build/plugin.AddSrcComment.js
@@ -6,7 +6,7 @@ export default function addSourceComment() {
     transform(code, id) {
       if (id.endsWith('.js') || id.endsWith('.ts')) {
         const rootDir = process.cwd();
-        const relativePath = path.relative(rootDir, id);
+        const relativePath = path.relative(rootDir, id).substring(4);;
         const comment = `// ${relativePath}\n`;
         return {
           code: comment + code,
---
+++ src/forms/example-form/RuleB.ts
@@ -0,0 +1,4 @@
+import suh from "core/SomeDependency"
+
+
+console.log(`from form-1: ${suh()}`)
